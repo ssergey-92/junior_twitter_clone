3be45c4c4aa5716c77671d103a969797fd4cb8a4
sergey <solop1992@mail.ru>
2024-05-14 17:22:13 +0300

    build: run_linters.py

---
+++ run_linters.py
@@ -0,0 +1,23 @@
+"""Check application 'Junior Twitter Clone' by linters.
+
+Install libraries for development from server/prod_requirements.txt than start
+linter wemake-python-styleguide and mypy.
+"""
+from subprocess import run as subprocess_run
+
+
+if __name__ == "__main__":
+    subprocess_run(
+        args="pip install -r server/dev_requirements.txt && "
+        "echo ===================================================;"
+        "echo Starting wemake-python-styleguide;"
+        "flake8 main.py run_pytests.py run_linters.py server/app;"
+        "echo ===================================================;"
+        "echo Starting mypy;"
+        "cd server/app &&"
+        "mypy --explicit-package-bases .;",
+        shell=True,
+        stdout=True,
+        stderr=True,
+        encoding="utf-8",
+    )
